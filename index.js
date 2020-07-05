597faac57ecacf900b9351bab1e8d36bf30e8414
Santosh Kumar <santoshkumar964887@gmail.com>
2020-07-05 18:38:13 +0530

    reading data.json file and sending as api

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-var url=require('url')
+var url=require('url');
 var http=require('http');
+var fs =require('fs');
+var data=fs.readFileSync(`${__dirname}/data.json`,'utf-8');
 var server=http.createServer((req,res)=>{
     var patthName=req.url;
+
     if(patthName==='/' || patthName==='/overview'){
         res.end('route overview');
 
 
     }
     else if(patthName==='/product') res.end('route product');
+    else if(patthName==='/api'){
+        res.end(data)
+
+    }
     else{
         res.writeHead(404,{
             'content-type':'text/html',
