76a34457527e28f537b1a3cfeec35a49f0e7b021
Santosh Kumar <santoshkumar964887@gmail.com>
2020-07-05 21:17:31 +0530

    creating templets

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 var url=require('url');
 var http=require('http');
 var fs =require('fs');
-var data=fs.readFileSync(`${__dirname}/data.json`,'utf-8');
+const data=fs.readFileSync(`${__dirname}/data.json`,'utf-8');
+const overviewTemplete=fs.readFileSync(`${__dirname}/templates/overview.html`,'utf-8');
+const productTemplete=fs.readFileSync(`${__dirname}/templates/product.html`,'utf-8');
+const templet_overviewTemplete=fs.readFileSync(`${__dirname}/templates/templet_overview.html`,'utf-8');
+
+const dataobj=JSON.parse(data);
 var server=http.createServer((req,res)=>{
     var patthName=req.url;
 
